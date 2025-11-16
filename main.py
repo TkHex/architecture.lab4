91cbc3033e47b2a7b1eb769abee82d439c01839e
TkHex <hcelovek2842@gmail.com>
2025-11-16 19:58:45 +0300

    add postgresql

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import psycopg
+import psycopg2
 import os
 from urllib.parse import urlparse
 
@@ -7,7 +7,14 @@ app = Flask(__name__)
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    conn = psycopg.connect(DATABASE_URL)
+    url = urlparse(DATABASE_URL)
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
 else:
     conn = None
 
